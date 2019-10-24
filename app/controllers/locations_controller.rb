a22badb064e3def2dfbfe2ed6156243e293128f8
adamcruser <adamjacobcruse@gmail.com>
2019-10-23 17:08:23 -0700

    fixed rubocops

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,18 +19,22 @@ class LocationsController < ApplicationController
     if @location.update(location_params)
       render :show
     else
-      render json: {error: "could not update"}, status: 400
+      render json: { error: "could not update" }, status: 400
     end
   end
 
   def destroy
-    @location =Location.find(params[:id])
+    @location = Location.find(params[:id])
     @location.destroy
   end
 
-private
+  private
 
- def location_params
-   params.require(:location).permit(:name, :location_type, :contact_name,:contact_phone_number,:contact_email,:contact_relationship,:address_street,:address_city,:address_state,:address_zip,:phone_number ,:reference,:school_principal,:principal_email,:school_distric,:county_district,:city_district)
- end
+  def location_params
+    params.require(:location).permit(:name, :location_type, :contact_name,
+                                     :contact_phone_number, :contact_email, :contact_relationship,
+                                     :address_street, :address_city, :address_state, :address_zip,
+                                     :phone_number, :reference, :school_principal, :principal_email,
+                                     :school_distric, :county_district, :city_district)
+  end
 end
