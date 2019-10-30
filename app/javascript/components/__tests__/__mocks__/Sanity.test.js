1ef27b3d57f71c8dd8a6092d5cf3188722fcd153
adamcruser <adamjacobcruse@gmail.com>
2019-10-30 14:39:13 -0700

    fixed pathings for jest testing and svg imports

--- app/javascript/components/__tests__/__mocks__/Sanity.test.js
+++ app/javascript/components/__tests__/__mocks__/Sanity.test.js
@@ -3,7 +3,7 @@ import Adapter from "enzyme-adapter-react-16";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "../../App";
+import { App } from "../../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 jest.mock("../../api/");
