d68c6380b921d6dbcbfae8d34c47ac14e7514b1b
rob <rosborn92@gmail.com>
2019-11-01 11:13:47 -0700

    yarn lint ran

--- app/javascript/components/AddBooking.js
+++ app/javascript/components/AddBooking.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Redirect } from "react-router-dom"
+import { Redirect } from "react-router-dom";
 
-class AddBooking extends React.Component{
+class AddBooking extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       form: {
         location: "",
@@ -13,90 +13,91 @@ class AddBooking extends React.Component{
         contact_email: "",
         contact_relationship: ""
       }
-    }
+    };
   }
 
-  handleChange = (e) => {
-    const { form } = this.state
-    const { name, value } = e.target
-    form[name] = value
-    this.setState({ form })
-  }
+  handleChange = e => {
+    const { form } = this.state;
+    const { name, value } = e.target;
+
+    form[name] = value;
+    this.setState({ form });
+  };
 
   handleClick = () => {
-    const{ form } = this.state
-    this.props.handleNewLocation(form)
-  }
+    const { form } = this.state;
+
+    this.props.handleNewLocation(form);
+  };
 
   render() {
-    const{
+    const {
       location,
       date,
       contact_name,
       contact_phone_number,
       contact_email,
-      contact_relationship,
-    } = this.state
-    const { success } = this.props
-    console.log(this.state.form)
-  return(
-    <React.Fragment>
+      contact_relationship
+    } = this.state;
+    const { success } = this.props;
+
+    console.log(this.state.form);
+    return (
       <form>
         <h1>Book an Event</h1>
-          <p>Select Location:</p>
-          <select
-            name="location"
-            value={location}
-            onChange={this.handleChange}>
-              {this.props.locations.map((location) => {
-                return(
-                  <option key={location.id} value={location.id}>{location.name}</option>
-                )
-              })}
-          </select>
+        <p>Select Location:</p>
+        <select name="location" onChange={this.handleChange} value={location}>
+          {this.props.locations.map(location => {
+            return (
+              <option key={location.id} value={location.id}>
+                {location.name}
+              </option>
+            );
+          })}
+        </select>
         <p>Enter Date of Event:</p>
         <input
-        type="date"
-        name="date"
-        value={date}
-        onChange={this.handleChange}
+          name="date"
+          onChange={this.handleChange}
+          type="date"
+          value={date}
         />
         <p>Enter Contact Name:</p>
         <input
-        type="text"
-        name="contact_name"
-        value={contact_name}
-        onChange={this.handleChange}
+          name="contact_name"
+          onChange={this.handleChange}
+          type="text"
+          value={contact_name}
         />
         <p>Enter Contact Phone Number:</p>
         <input
-          type="text"
           name="contact_phone_number"
-          value={contact_phone_number}
           onChange={this.handleChange}
+          type="text"
+          value={contact_phone_number}
         />
         <p>Enter Contact Email:</p>
         <input
-          type="text"
           name="contact_email"
-          value={contact_email}
           onChange={this.handleChange}
+          type="text"
+          value={contact_email}
         />
         <p>Enter Contact Relationship to Location:</p>
         <input
-          type="text"
           name="contact_relationship"
-          value={contact_relationship}
           onChange={this.handleChange}
+          type="text"
+          value={contact_relationship}
         />
         <br />
         <br />
-        <button type="button" onClick={this.handleClick}>Submit</button>
-        {success && <Redirect to="/" /> }
+        <button onClick={this.handleClick} type="button">
+          Submit
+        </button>
+        {success && <Redirect to="/" />}
       </form>
-
-    </React.Fragment>
-  )
-}
+    );
+  }
 }
 export default AddBooking;
